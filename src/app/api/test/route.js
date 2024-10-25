c80d28777b41dae861a08b748133770a6e9558d3
NEERAJ SINGH RAJPUT <neeraj.rajput@tinyscript.in>
2024-10-25 18:26:36 +0530

    test 5

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,14 +1,6 @@
-import Expense from "@/model/Expense.js";
-
 export async function GET() {
-  console.log(process.env.MOONGOOSE_URI);
-  const expenses = await Expense.find();
-  console.log(expenses);
-  return new Response(
-    JSON.stringify({ message: "API is working!", expenses }),
-    {
-      status: 200,
-      headers: { "Content-Type": "application/json" },
-    }
-  );
+  return new Response(JSON.stringify({ message: "API is working!" }), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
 }
